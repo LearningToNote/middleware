5fa175187e41bf707785f70a8cf794d6280a8379
Maximilian Grundke <maximiliangrundke@googlemail.com>
2015-10-27 21:32:18 +0100

    Enable document retrieval

--- server.py
+++ server.py
@@ -1,3 +1,5 @@
+import json, pyhdb, os
+
 from flask import Flask
 from flask.ext.cors import CORS
 
@@ -5,10 +7,35 @@ from flask.ext.cors import CORS
 app = Flask(__name__)
 CORS(app)
 
+SERVER_ROOT = os.path.dirname(os.path.realpath(__file__))
+connection = None
 
-@app.route('documents/')
+def init():
+    global connection
+
+    with open(SERVER_ROOT + "/secrets.json") as f:
+        secrets = json.load(f)
+
+    connection = pyhdb.connect(
+        host=secrets['host'],
+        port=secrets['port'],
+        user=secrets['username'],
+        password=secrets['password']
+    )
+
+    app.run(port=8080, debug=True)
+
+
+@app.route('/documents')
 def get_documents():
-    pass
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM MP12015.DOCUMENT")
+    documents = list()
+    for result in cursor.fetchall():
+        documents.append(result[0])
+    cursor.close()
+    return json.dumps(documents)
+
 
 
 @app.route('/documents/<id>')
@@ -17,4 +44,4 @@ def get_document(id):
 
 
 if __name__ == '__main__':
-    app.run(port=4242, debug=True)
+    init()
