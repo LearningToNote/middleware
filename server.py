4ce13179d4552b8d5e2a972e718dc33d30a73e81
Jan Philipp Sachse <jan.sachse@student.hpi.de>
2015-10-30 16:32:49 +0100

    Start to adjust the middleware to our new schema

--- server.py
+++ server.py
@@ -29,7 +29,7 @@ def init():
 @app.route('/documents')
 def get_documents():
     cursor = connection.cursor()
-    cursor.execute("SELECT * FROM MP12015.DOCUMENT")
+    cursor.execute("SELECT id FROM LEARNING_TO_NOTE.DOCUMENTS")
     documents = list()
     for result in cursor.fetchall():
         documents.append(result[0])
@@ -37,21 +37,54 @@ def get_documents():
     return respond_with(documents)
 
 
-@app.route('/documents/<id>')
-def get_document(id):
+@app.route('/documents/<document_id>')
+def get_document(document_id):
     cursor = connection.cursor()
-    cursor.execute("SELECT TEXT FROM MP12015.SENTENCE WHERE DOCUMENT_ID = ?", (id,))
-    sentences = list()
-    for result in cursor.fetchall():
-        sentences.append(str(result[0]))
+    result = {}
+    result['text'] = get_text(cursor, document_id)
+    result['denotations'] = get_denotations(cursor, document_id)
+    result['relations'] = get_relations(cursor, document_id)
     cursor.close()
-    return respond_with(" ".join(sentences))
+    return respond_with(result)
 
+def get_text(cursor, document_id):
+    cursor.execute("SELECT TEXT FROM LEARNING_TO_NOTE.DOCUMENTS WHERE ID = ?", (document_id,))
+    text = str(cursor.fetchone()[0])
+    return text
+
+def get_denotations(cursor, document_id):
+    cursor.execute('SELECT E.ID, E."TEXT", E."TYPE", DE."START", DE."END" FROM LEARNING_TO_NOTE.DOC_ENTITIES DE \
+        JOIN LEARNING_TO_NOTE.ENTITIES E ON DE.ENTITY_ID = E.ID AND DE.DOC_ID = ?', (document_id,))
+    denotations = []
+    for result in cursor.fetchall():
+        denotation = {}
+        denotation['id'] = str(result[0])
+        denotation['span'] = {}
+        denotation['span']['begin'] = str(result[3])
+        denotation['span']['end'] = str(result[4])
+        denotation['obj'] = str(result[1])
+        denotation['type'] = str(result[2])
+        denotations.append(denotation)
+    return denotations
+
+def get_relations(cursor, document_id):
+    cursor.execute("SELECT PAIRS.ID, E1_ID, E2_ID, TYPE, DDI FROM LEARNING_TO_NOTE.PAIRS \
+        JOIN LEARNING_TO_NOTE.DOC_ENTITIES DE1 ON PAIRS.E1_ID = DE1.ID AND DE1.DOC_ID = ? \
+        JOIN LEARNING_TO_NOTE.DOC_ENTITIES DE2 ON PAIRS.E2_ID = DE2.ID AND DE2.DOC_ID = ?", (document_id, document_id,))
+    relations = []
+    for result in cursor.fetchall():
+        relation = {}
+        relation['id'] = str(result[0])
+        relation['pred'] = str(result[3])
+        relation['subj'] = str(result[1])
+        relation['obj'] = str(result[2])
+        relations.append(relation)
+    return relations
 
 @app.route('/sentences')
 def get_sentences():
     cursor = connection.cursor()
-    cursor.execute("SELECT * FROM MP12015.SENTENCE")
+    cursor.execute("SELECT * FROM LEARNING_TO_NOTE.SENTENCE")
     sentences = list()
     for result in cursor.fetchall():
         sentences.append(result[0])
@@ -62,7 +95,7 @@ def get_sentences():
 @app.route('/sentences/<sentence_id>')
 def get_sentence(sentence_id):
     cursor = connection.cursor()
-    cursor.execute("SELECT TEXT FROM MP12015.SENTENCE WHERE SENTENCE.ID = ?", (sentence_id,))
+    cursor.execute("SELECT TEXT FROM LEARNING_TO_NOTE.SENTENCE WHERE SENTENCE.ID = ?", (sentence_id,))
     sentence = cursor.fetchone()[0]
     cursor.close()
     return respond_with(str(sentence))
@@ -72,7 +105,7 @@ def get_sentence_entities(sentence_id):
     cursor = connection.cursor()
     cursor.execute(
     'SELECT ENTITY.TEXT, OFFSET."START", OFFSET."END" '
-    'FROM MP12015.ENTITY, MP12015.OFFSET '
+    'FROM LEARNING_TO_NOTE.ENTITY, LEARNING_TO_NOTE.OFFSET '
     'WHERE ENTITY.ID = OFFSET.ENTITY_ID '
     'AND ENTITY.SENTENCE_ID = ?', (sentence_id,))
 
